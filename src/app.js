05e3083bbb1d545fb272212f131e8d8f0ecf6fa6
skate <ilagnev@gmail.com>
2017-11-26 19:46:39 +0200

    simple component bond

---
+++ src/app.js
@@ -0,0 +1,10 @@
+console.log(`hello i'm webpacked 🌚`);
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Greeting from "./greeting.jsx";
+
+ReactDOM.render(
+    <Greeting/>,
+    document.getElementById('root')
+);
