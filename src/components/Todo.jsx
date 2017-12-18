7dd9a7023fa8479f12cff3552ad3e01216f861a2
skate <ilagnev@gmail.com>
2017-12-18 15:45:51 +0200

    style todo item, add remove button

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import * as TodoActions from '../actions/TodoActions';
+import './Todo.css';
 
 export default class Todo extends React.Component {
     constructor(props) {
@@ -8,6 +9,7 @@ export default class Todo extends React.Component {
         this.state = {todo: props.todo};
 
         this.toggleTodo = this.toggleTodo.bind(this);
+        this.removeTodo = this.removeTodo.bind(this);
     }
 
     toggleTodo(e) {
@@ -25,20 +27,34 @@ export default class Todo extends React.Component {
             ? TodoActions.completeTodo(newTodo.id)
             : TodoActions.uncompleteTodo(newTodo.id)
     }
+
+    removeTodo(e) {
+        e.preventDefault();
+        console.log('remove todo: ', this.state.todo);
+        TodoActions.removeTodo(this.state.todo.id);
+    }
     
     render() {
         const { todo } = this.state;
         const checked = todo.complete ? 'checked' : '' ;
 
         return (
-            <li class="list-group-item">
-                <span class="badge">{+todo.complete}</span>
+            <li class="list-group-item todo-item">
                 <input 
                     type="checkbox" 
                     onChange={this.toggleTodo}
                     id={todo.id} 
                     checked={checked} />
-                {todo.id} {todo.title} 
+                <label for={todo.id}>
+                    {todo.id} {todo.title} 
+                    {/* <span class="badge"><a href="#" class="btn btn-info">X</a></span> */}
+                    <button 
+                        onClick={this.removeTodo}
+                        type="button"
+                        class="btn btn-link btn-sm pull-right" >
+                        <i class="fa fa-remove" aria-hidden="true"></i>
+                    </button>
+                </label>
             </li>
         )
     }
